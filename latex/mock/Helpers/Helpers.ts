23fe45562978a831550c9916f1bf127701bcbe8f
radhanama <radhanama@hotmail.com>
2023-06-10 19:59:45 -0300

    style

--- latex/mock/Helpers/Helpers.ts
+++ latex/mock/Helpers/Helpers.ts
@@ -1,5 +1,9 @@
 export class Helpers {
     public static sleep(t: number, t2: number) : Promise<void> {
-        return new Promise(r=>setTimeout(r,~~ (Math.random()*(t2-t)+t)));
+        return new Promise(
+            r => setTimeout(
+                r,~~ (Math.random()*(t2-t)+t)
+            )
+        );
     }
 }
