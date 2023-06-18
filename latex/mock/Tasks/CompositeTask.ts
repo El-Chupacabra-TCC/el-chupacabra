efb6c048340cdcbffa73f31896a84ec8ec4ca41c
Matheus Moura <matheus.dsmoura@protonmail.com>
2023-06-18 19:08:52 -0300

    Refatorando cap poc p1.

--- latex/mock/Tasks/CompositeTask.ts
+++ latex/mock/Tasks/CompositeTask.ts
@@ -2,26 +2,25 @@ import { Task } from "./Task";
 
 export class CompositeTask extends Task {
     private tasks: Task[];
-  
+
     constructor() {
-      super("Composite Task", []);
-      this.tasks = [];
+        super("Composite Task", []);
+        this.tasks = [];
     }
-  
+
     addTask(task: Task): void {
-      this.tasks.push(task);
-      task.metrics.forEach(element => {
-        this.metrics.add(element);
-      });
+        this.tasks.push(task);
+        task.metrics.forEach(element => {
+            this.metrics.add(element);
+        });
     }
 
     async execute(): Promise<any> {
-      console.log("Executing composite task...");
-      let tasks = {};
-      for (const task of this.tasks) {
-        tasks[task.name] = await task.run();
-      }
-      return tasks;
+        console.log("Executing composite task...");
+        let tasks = {};
+        for (const task of this.tasks) {
+            tasks[task.name] = await task.run();
+        }
+        return tasks;
     }
-  }
-  
+}
