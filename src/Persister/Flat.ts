43c5737588bd1c9392ee315a8d1d4ea87fcdb8fc
radhanama <radhanama@hotmail.com>
2023-08-25 00:56:07 +0000

    documentation

--- src/Persister/Flat.ts
+++ src/Persister/Flat.ts
@@ -1,4 +1,12 @@
+/**
+ * A utility class for flattening nested objects.
+ */
 export default class Flat {
+  /**
+   * Checks if an object is a buffer.
+   * @param obj - The object to check.
+   * @returns `true` if the object is a buffer, otherwise `false`.
+   */
   private static isBuffer(obj: any): boolean {
     return (
       obj &&
@@ -8,14 +16,31 @@ export default class Flat {
     );
   }
 
+  /**
+   * Identity function that returns the input key as is.
+   * @param key - The key to transform.
+   * @returns The input key unchanged.
+   */
   private static keyIdentity<T>(key: T): T {
     return key;
   }
 
+  /**
+   * Flattens a nested object.
+   * @param target - The object to flatten.
+   * @param opts - Options for the flattening process.
+   * @returns The flattened object.
+   */
   public static flatten(target: Record<string, any>, opts: FlattenOptions = {}): Record<string, any> {
     const { delimiter = '.', maxDepth, transformKey = this.keyIdentity, safe } = opts;
     const output: Record<string, any> = {};
 
+    /**
+     * Recursively steps through the object and flattens it.
+     * @param object - The object to flatten.
+     * @param prev - The prefix for the current key.
+     * @param currentDepth - The current depth in the object hierarchy.
+     */
     const step = (object: Record<string, any>, prev = '', currentDepth = 1): void => {
       Object.keys(object).forEach((key) => {
         const value = object[key];
@@ -43,6 +68,9 @@ export default class Flat {
   }
 }
 
+/**
+ * Options for the flattening process.
+ */
 interface FlattenOptions {
   delimiter?: string;
   maxDepth?: number;
